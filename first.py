5b98257590c135069d242b5902ec02fcf8874876
Joelk97 <kuehljoel@gmail.com>
2023-01-25 08:40:59 +0100

    first

--- first.py
+++ first.py
@@ -1,1 +1,7 @@
-print("welcome")
+import pandas as pd
+
+df = pd.read_excel(
+    r'C:\Users\joel.kuhl\Documents\tutorials\tut-vba-py\marks.xlsx', engine="openpyxl")
+
+results = df.columns
+print(results)
